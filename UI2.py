476cc7c15880f40558075fa55aeaf06a956ac388
jela3105 <jela3105@gmail.com>
2024-06-12 21:20:33 -0600

    Revert "Plot senal binaria y mostar imagenes"
    This reverts commit 41f6eb427c8740d22717bbd712cf440069bf8f69.

--- UI2.py
+++ UI2.py
@@ -5,26 +5,18 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 from PIL import Image, ImageTk
 
-class GUI2:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Demodulador PCM")
-        self.crear_widgets()
-
-    # Función para crear el gráfico del seno
-    def crear_senal_modulada(self, parent):
-        fig, ax = plt.subplots(figsize=(12, 3))
-        with open('audio_codigo_pcm.txt', 'r') as file:
-            # Read the first 10 characters
-            primeros_10_chars= file.read(10)
-            primeros_bits = [int(char) for char in primeros_10_chars if char.isdigit()]
-        ax.plot(primeros_bits)
-        ax.set_title('Senal modulada (codigo binario)', fontsize=8)
-        ax.tick_params(axis='both', which='major', labelsize=6)
-        ax.tick_params(axis='both', which='minor', labelsize=6)
-        self.canvas = FigureCanvasTkAgg(fig, master=parent)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.LEFT)
+# Función para crear el gráfico del seno
+def create_sin_plot(parent):
+    fig, ax = plt.subplots(figsize=(12, 3))
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    ax.plot(x, y)
+    ax.set_title('Seno', fontsize=8)
+    ax.tick_params(axis='both', which='major', labelsize=6)
+    ax.tick_params(axis='both', which='minor', labelsize=6)
+    canvas = FigureCanvasTkAgg(fig, master=parent)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.LEFT)
 
     # Función para crear el gráfico del coseno
     def create_cos_plot(self, parent):
@@ -57,10 +49,10 @@ class GUI2:
         self.frame_row1 = tk.Frame(self.root)
         self.frame_row1.pack(fill=tk.X, pady=5)
 
-        self.dropdown1 = ttk.Combobox(self.frame_row1, values=["Imagen", "Audio"])
-        self.dropdown1.pack(side=tk.LEFT, padx=10)
-        
-        self.crear_senal_modulada(self.frame_row1)
+    dropdown1 = ttk.Combobox(frame_row1, values=["Imagen", "Audio"])
+    dropdown1.pack(side=tk.LEFT, padx=10)
+    
+    crear_senal_modulada(frame_row1)
 
         # Segunda fila: Dropdown y gráfico de coseno
         self.frame_row2 = tk.Frame(self.root)
@@ -75,8 +67,8 @@ class GUI2:
         self.frame_row3 = tk.Frame(self.root)
         self.frame_row3.pack(fill=tk.X, pady=10)
 
-        self.create_image_label(self.frame_row3, "imagen_gris.jpg")  
-        self.create_image_label(self.frame_row3, "imagen_cuantizada.jpg")  
+    create_image_label(frame_row3, "imagen_gris.jpg")  
+    create_image_label(frame_row3, "imagen_cuantizada.jpg")  
 
         self.dropdown3 = ttk.Combobox(self.frame_row3, values=["BPSK", "8PSK", "16QAM"])
         self.dropdown3.pack(side=tk.LEFT, padx=10)
